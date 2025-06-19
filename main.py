2ae1b28c694989cf941b208ec7cea2cca752bad6
johnpradeep06 <johnpradeep100@gmail.com>
2025-06-19 20:27:57 +0530

    changed the credentials.json directory

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ if api and api.startswith("'") and api.endswith("'"):
 
 # ------------------- SHEET SETUP -------------------
 scope = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
-creds = Credentials.from_service_account_file("resources/credentials.json", scopes=scope)
+creds = Credentials.from_service_account_file("credentials.json", scopes=scope)
 client = gspread.authorize(creds)
 sheet = client.open("Quote_Access").sheet1
 
